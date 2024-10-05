c7aea383f85cd55fc266b36ddac502d576d6ddd7
methos87 <novakakos87@gmail.com>
2024-10-05 02:11:04 +0200

    First simple case

--- src/sumofpositive.py
+++ src/sumofpositive.py
@@ -2,11 +2,7 @@
 
 
 def positive_sum(arr):
-	result = 0	
-	for i in arr:
-        if i > 0:
-            reuslt += i
-	return result
+	return sum(i for i in arr if i > 0)
 
 
 print(positive_sum([1, 2, 3, 4, 5])) # 15

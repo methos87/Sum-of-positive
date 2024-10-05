718a7039dd6ac92ef25e2d7f784a792f7930b11e
methos87 <novakakos87@gmail.com>
2024-10-05 02:07:23 +0200

    Error corrections 03

--- src/sumofpositive.py
+++ src/sumofpositive.py
@@ -1,14 +1,13 @@
-!#/usr/bin/env python
+#!/usr/bin/env python
 
 
 def positive_sum(arr):
 	result = 0	
 	for i in arr:
-		if i >= 0:
-			result += i
-		else:
-			result += 0
+        if i > 0:
+            reuslt += i
 	return result
 
 
-positive_sum([1, 2, 3, 4, 5]) # 15
+print(positive_sum([1, 2, 3, 4, 5])) # 15
+
